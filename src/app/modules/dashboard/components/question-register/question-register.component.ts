9175ccf5bbf2e60875cd35ab9f584b8117f45dde
CarlosT_T <carlos.junior@zup.com.br>
2019-09-24 14:34:01 -0500

    Register question

--- src/app/modules/dashboard/components/question-register/question-register.component.ts
+++ src/app/modules/dashboard/components/question-register/question-register.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-question-register',
   templateUrl: './question-register.component.html',
   styleUrls: ['./question-register.component.css']
 })
+
 export class QuestionRegisterComponent implements OnInit {
+  public questionForm: FormGroup;
+  constructor( private formBuilder : FormBuilder) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  onSubmit(){
+    console.log(this.questionForm.value)
   }
 
+  ngOnInit() {
+    this.questionForm = this.formBuilder.group({ 
+      question: new FormControl("", Validators.compose([Validators.required])),
+      answer: new FormControl("", Validators.compose([Validators.required])),
+      alternative1: new FormControl("", Validators.compose([Validators.required])),
+      alternative2: new FormControl("", Validators.compose([Validators.required])),
+      alternative3: new FormControl("", Validators.compose([Validators.required])),
+      alternative4: new FormControl("", Validators.compose([Validators.required]))
+    })
+  }  
 }
--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { QuestionRegisterComponent } from './components/question-register/question-register.component';
-
+import { ReactiveFormsModule, FormsModule } from "@angular/forms"
 
 
 @NgModule({
   declarations: [QuestionRegisterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+   FormsModule,
+   ReactiveFormsModule
   ]
 })
 export class DashboardModule { }
